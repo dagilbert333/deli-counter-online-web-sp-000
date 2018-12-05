e59e5fa66d3b37ddc85141bcb9a2ea8ab2b5e1f1
David Gilbert <dagilbert333@gmail.com>
2018-12-05 00:21:03 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(katz_deli)
+  line = [];
+  i = 0;
+  while i < katzDeliLine.length {
+  line.push(` `+[i+1]+`. `  + katzDeliLine[i])
+  i++;
+  }
+if (katzDeliLine.length === 0) {
+  return "The line is currently empty.";
+   } else
+   return(`The line is currently:` + line);
+} 
+end
