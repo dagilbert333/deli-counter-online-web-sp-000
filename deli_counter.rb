01e6044c864ce8fac3b30cc8ee29dace28ebf2fb
David Gilbert <dagilbert333@gmail.com>
2018-12-07 20:06:59 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,8 @@
-# Write your code here.
+def line(katz_deli)
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
+  elsif katz_deli.size > 0 
+    
+    "The line is currently: "
+  end
+end
