1b2eebb70da07ea2e079a76c3cf9addd97633cc3
David Gilbert <dagilbert333@gmail.com>
2018-12-11 00:09:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
-# Write your code here.
+def line(katz_deli)
+  message = "The line is currently: "
+  line_message = []
+  if katz_deli.size >= 1 
+    katz_deli.each_with_index do |person, index| 
+      line_message << "#{index+1}. #{person}"
+    end
+  puts message + line_message.join(" ")
+  else
+    puts "The line is currently empty."
+  end
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli << name
+    puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+end
